4ab5a05346c694fe519e85cb31a58157b362c231
高俊峰 <gaojunfeng@gaojunfengdeMacBook-Pro.local>
2019-12-23 23:48:30 +0800

    add

--- lib/widget/seach_bar/index.dart
+++ lib/widget/seach_bar/index.dart
@@ -16,45 +16,41 @@ class _SeachBarState extends State<SeachBar> {
           // color: _activeColor,
         ),
         Expanded(
-          child: ClipRRect(
-            // 设置圆角
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            // 填充
-            child: Container(
-              // height: 40,
-              decoration: BoxDecoration(
-                // 边色与边宽度
-                border: Border.all(color: Color(0xfff2f2f2), width: 1),
-                // 颜色
-                // color: Colors.red,
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    TextFormField(
-                      style: TextStyle(
-                        
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        // backgroundColor: Colors.yellow,
-                        // height: 1,
-                      ),
-                      decoration: InputDecoration(
-                        // contentPadding: EdgeInsets.symmetric(vertical: 0.0),
-                        hintText: '请输入内容', // 默认回显
-                        // border: InputBorder.none, // 无边框
-                      ),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
+          // ConstrainedBox即创建一个Widget，该Widget对其子child施加附加约束。之前在constrain也有简单使用，即该组件一般集成在constrain中使用。
+          // http://www.imooc.com/article/286213
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: 25,
+              // maxWidth: 200,
+            ),
+            child: Form(
+              key: _formKey,
+              // 使用 Form 表单后，使用 TextFormField 代替  TextField 输入框
+              child: TextFormField(
+                // InputDecoration: 设置输入框的样式修饰器
+                decoration: InputDecoration(
+                  // 设置内容填充，设置对称关系
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 2.0, // 上下对称
+                    horizontal: 10.0, // 水平左右对称
+                  ),
+                  hintText: '请输入搜索内容',
+                  // prefixIcon: Icon(Icons.search),
+                  // contentPadding: EdgeInsets.all(10),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15), // 设置圆角
+                    borderSide: BorderSide.none, // 设置边界线，没有
+                  ),
+                  filled: true, // 设置填满
+                  fillColor: Color(0xffaaaaaa), // 填充颜色
                 ),
+                // 校验
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
               ),
             ),
           ),
